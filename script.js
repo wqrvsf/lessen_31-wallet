2b062319be5a0a21d536d0c1f586555f69f7a3ac
wqrvsf <98298812+wqrvsf@users.noreply.github.com>
2023-05-20 15:43:34 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,24 +2,36 @@ let btn = document.querySelector('.editor__button');
 let name = document.querySelector('.name');
 let price = document.querySelector('.price');
 let category = document.querySelector('.category');
-let tbody = document.querySelector('#tbody');
+let tbody = document.querySelector('.tbody');
+let btnDel = document.querySelector('.del');
 
 let purchase = {};
 
-btn.addEventListener('click', function (event){
+btn.addEventListener('click', function (event) {
     event.preventDefault();
     purchase.name = form.name.value;
     purchase.price = form.price.value;
-    purchase.category = form.selectCategory.options[form.selectCategory.selectedIndex].innerText;
+    purchase.category = form.category.options[form.category.selectedIndex].innerText;
     tbody.insertAdjacentHTML('beforeEnd',
         `<tr class = "purchases__item purchases__row" data-category = "product">
             <td class = "purchases__td">${purchase.name}</td>
             <td class = "purchases__td">${purchase.category}</td>
             <td class = "purchases__td">${purchase.price}</td>
-            <td class = "purchases__td"><i class = "purchases__item-del fa-solid fa-xmark"></i></td>
+            <td class = "purchases__td del">✖</td>
         </tr>`
     )
 });
+
+tbody.addEventListener('click', (evt) => {
+    console.log(evt);
+    console.log(evt.target);
+    evt.target.classList.contains('del');
+    if (evt.target = true) {
+        evt.target.closest('tr').remove();
+    }
+    // проверить наличие класса "del" у элемента по которому кликнули
+    // если класс есть - удалить родителя tr (загуглить как найти родителя элемента)
+})
 
 
 
