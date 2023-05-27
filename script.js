4f346d9d2bcb774e8b601050f391554a30d51e1f
wqrvsf <98298812+wqrvsf@users.noreply.github.com>
2023-05-27 15:31:20 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -33,6 +33,49 @@ tbody.addEventListener('click', (evt) => {
     // если класс есть - удалить родителя tr (загуглить как найти родителя элемента)
 })
 
+let purchaseArray = [];
+let BTN = form.addBtn;
+let categories = document.querySelectorAll('.legend__desc');
+let prices = document.querySelectorAll('.legend__price');
+
+console.log(categories);
+
+BTN.addEventListener('click', (event) => {
+    let purchase = {};
+    event.preventDefault();
+    purchase.name = form.name.value;
+    purchase.price = +form.price.value;
+    let options = form.category.options;
+    purchase.category = options[form.category.selectedIndex].innerText;
+    for (let category of categories) {
+        if(purchase.category == category.innerText) {
+            category.nextElementSibling.textContent = Number(category.nextElementSibling.innerText) + purchase.price;
+        }
+    }
+    purchaseArray.push(purchase);
+    console.log(purchaseArray);
+
+    tbody.insertAdjacentHTML('beforeEnd',
+    `
+    <tr class = "purchases__item purchases__row" data-category = "product">
+        <td class = "purchases__td">${purchase.name}</td>
+        <td class = "purchases__td">${purchase.category}</td>
+        <td class = "purchases__td">${purchase.price}</td>
+        <td class = "purchases__td"><i class = "purchases__item-del fa-solid fa-xmark"></i></td>
+    </tr>
+    `
+    )
+    form.reset();
+});
+
+tbody.addEventListener('click', (event) => {
+    if (event.target.classList.contains('purchases__item-del')){
+        event.target.closest('tr').remove();
+    }
+})
+
+
+
 
 
 
